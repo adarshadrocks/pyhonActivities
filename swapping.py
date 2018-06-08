d2cbd330aee26cc004248eaad1aaa540b0108f87
ADARSH MISHRA <adarshmishra067@gmail.com>
2018-06-08 23:43:29 +0530

    Add files via upload
    swapping of two no.by taking inputs from user

---
+++ swapping.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+'''swapping of two no.s'''
+x=raw_input("enter the first no.")
+y=raw_input("enter the second no.")
+x,y=y,x
+print "after swapping"
+print "first no. :"+x
+print "second no. :"+y
