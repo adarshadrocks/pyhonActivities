79065393f52d44fcb1c0e298d6ee09f9234dadc0
adarsh <adarshmishra067@gmail.com>
2018-06-08 23:53:12 +0530

    1st.py

---
+++ 1st.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import time
+import os
+import webbrowser
+option='''
+press 1 : current date and time
+press 2 : To create a file
+press 3 : To create a directory
+press 4 : To search on google
+press 5 : logout your system
+press 6 : Shutdown your os
+press 7 : To check internet connection in your pc
+press 8 : To login watsapp on browser
+press 9 : To check all connected IP in your network
+'''
+print(option)
+raw_input("kindly press atleat one")
+
